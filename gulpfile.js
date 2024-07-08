958afad05789276b4b82f4671ed6164583bc601d
vladislav lyashkov <vladislav.lyaskov@gmail.com>
2024-07-08 13:04:14 +0300

    as

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 'use strict';
 const gulp = require('gulp');
-const uglify =require('gulp-uglify');
+const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 
-gulp.task('js-min',()=>{
+gulp.task('js-min', () => {
     return gulp.src('./MyMask.js')
         .pipe(uglify())
-        .pipe(rename({suffix:".min"}))
+        .pipe(rename({suffix: ".min"}))
         .pipe(gulp.dest('./'));
 });
 
-gulp.task("default",gulp.parallel('js-min'));
+gulp.task("default", gulp.parallel('js-min'));
