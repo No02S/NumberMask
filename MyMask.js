3c27c42684fda72648a1afa054e6437d543b686a
vladislav lyashkov <vladislav.lyaskov@gmail.com>
2024-07-08 13:14:36 +0300

    as

--- MyMask.js
+++ MyMask.js
@@ -1,28 +1,28 @@
-export const numberMask = ({form,input,always=false}) =>{
+export const numberMask = ({form, input, always = false}) => {
     let phoneMask = {
         mask: "+7 (___) ___-__-__",
-        lastNumber:''
+        lastNumber: ''
     };
     let phoneInput = form.querySelector(input);
 
     always ? phoneInput.value = phoneMask.mask : phoneInput.value = '';
 
-    phoneInput.addEventListener('focus',(e)=>{
+    phoneInput.addEventListener('focus', (e) => {
         e.target.value = phoneMask.mask;
     });
-    phoneInput.addEventListener('click',(e)=>{
-        e.target.setSelectionRange(phoneMask.mask.indexOf('_'),phoneMask.mask.indexOf('_'));
+    phoneInput.addEventListener('click', (e) => {
+        e.target.setSelectionRange(phoneMask.mask.indexOf('_'), phoneMask.mask.indexOf('_'));
     });
-    phoneInput.addEventListener('keydown',(e)=>{
+    phoneInput.addEventListener('keydown', (e) => {
 
         if (e.key == "Backspace" && phoneMask.lastNumber != "(") {
             e.preventDefault();
             phoneMask.mask = phoneMask.mask.split('');
             let indexLastNumber = phoneMask.mask.lastIndexOf(phoneMask.lastNumber);
 
-            for( let i = indexLastNumber-1;i>0;i--) {
+            for (let i = indexLastNumber - 1; i > 0; i--) {
                 phoneMask.lastNumber = phoneMask.mask[i];
-                if(phoneMask.lastNumber == '-' || phoneMask.lastNumber == ' ' ||  phoneMask.lastNumber == ')') {
+                if (phoneMask.lastNumber == '-' || phoneMask.lastNumber == ' ' || phoneMask.lastNumber == ')') {
                     continue;
                 } else {
                     break;
@@ -31,16 +31,16 @@ export const numberMask = ({form,input,always=false}) =>{
             phoneMask.mask[indexLastNumber] = '_';
             phoneMask.mask = phoneMask.mask.join('');
 
-        } else if(e.key != undefined && RegExp(/_/).test(phoneMask.mask) && (e.key).replace(/([^\d]|\,)/gi, '') != '') {
+        } else if (e.key != undefined && RegExp(/_/).test(phoneMask.mask) && (e.key).replace(/([^\d]|\,)/gi, '') != '') {
             phoneMask.mask = phoneMask.mask.replace(/_/, e.key);
             phoneMask.lastNumber = e.key;
         }
-        setTimeout(()=>{
+        setTimeout(() => {
             e.target.value = phoneMask.mask;
-            e.target.setSelectionRange(phoneMask.mask.indexOf('_'),phoneMask.mask.indexOf('_'));
+            e.target.setSelectionRange(phoneMask.mask.indexOf('_'), phoneMask.mask.indexOf('_'));
         });
     });
-    form.addEventListener('submit',()=>{
+    form.addEventListener('submit', () => {
         phoneMask.mask = "+7 (___) ___-__-__";
     });
 };
